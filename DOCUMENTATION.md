56ce8e98f33fe22d73e6bdee8fc673cc4671e4b0
Your Name <franzluehr@gmail.com>
2026-05-19 23:36:23 +0200

    docs: auto-sync v6.8.0

---
+++ DOCUMENTATION.md
@@ -0,0 +1,11 @@
+# visualCountDown API Reference
+Generated: 2026-05-19
+
+a node that will display a countdown on screen
+
+## Class: CountDown2D
+**Inherits:** [NumbersUI2D](git@github.com:ChillCube/NumberDisplay/blob/main/DOCUMENTATION.md)
+
+
+---
+
---
+++ countdownNode.gd
@@ -0,0 +1,50 @@
+@tool
+extends NumbersUI2D
+class_name CountDown2D
+
+@export var count : int = 10:
+	set(val):
+		count = val
+		number = val
+@export var count_length : float = 1.0
+@export var autostart : bool = false
+@export var one_shot : bool = false
+
+signal countdown_finished()
+
+var _running : bool = false
+var _elapsed : float = 0.0
+
+func _ready() -> void:
+	number = count
+
+	if Engine.is_editor_hint():
+		return
+
+	if autostart:
+		_running = true
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint() or not _running:
+		return
+	_elapsed += delta
+	if _elapsed >= count_length:
+		_elapsed -= count_length
+		count_down()
+
+func start() -> void:
+	number = count
+	_elapsed = 0.0
+	_running = true
+
+func stop() -> void:
+	_running = false
+
+func count_down() -> void:
+	var new_number = number - 1
+	if new_number >= 0:
+		number = new_number
+	else:
+		if one_shot:
+			_running = false
+		emit_signal("countdown_finished")
